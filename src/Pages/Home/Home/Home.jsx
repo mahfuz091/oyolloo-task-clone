9b00baa2d170a984ff8d0612c74b42cb65886841
mahfuz091 <engmahfuz14@gmail.com>
2023-07-20 03:17:58 +0600

    partners sections added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Banner from "../../Banner/Banner";
+import Partners from "../../Partners/Partners";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Partners></Partners>
     </div>
   );
 };
