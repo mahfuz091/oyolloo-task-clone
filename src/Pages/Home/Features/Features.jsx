4ae6d3894fffe3d87bc1a0e2a45a27cf5d475223
mahfuz091 <engmahfuz14@gmail.com>
2023-07-20 10:26:46 +0600

    feature section update

--- src/Pages/Home/Features/Features.jsx
+++ src/Pages/Home/Features/Features.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import feature from "../../../assets/Image/feature.png";
 
 const Features = () => {
   return (
-    <div className='mt-[140px] pl-[299px] pr-[250px]'>
+    <div className='mt-[140px] pl-[299px] pr-[250px] flex gap-[83px]'>
       <div>
         <h2 className='feature-text flex items-center gap-2'>
           <hr className='line1' />
@@ -11,6 +12,17 @@ const Features = () => {
         <h1 className='feature-title'>
           Solve your business problems with our experts
         </h1>
+      </div>
+      <div>
+        <p className='feature-description mb-[38px]'>
+          Business consultants provide management consulting to help businesses
+          and organizations improve performance and efficienc
+        </p>
+        <div className='relative'>
+          <img className='' src={feature} alt='' />
+          <div className='project-success'></div>
+          <div className='element absolute -bottom-[47px] -right-[50px] -z-10'></div>
+        </div>
       </div>
     </div>
   );
