90d0b9655a6ec3ba20c3e4a11e92b6dd711b74e1
mahfuz091 <engmahfuz14@gmail.com>
2023-07-21 12:51:20 +0600

    footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavbarOne from "../Pages/Home/Navbar/NavbarOne";
 import NavbarTwo from "../Pages/Home/Navbar/NavbarTwo";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Footer/Footer";
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       <NavbarOne></NavbarOne>
       <NavbarTwo></NavbarTwo>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
